4a73f518556f426084ec0fe10378203673585429
Sean McArthur <sean.monstar@gmail.com>
2011-06-24 14:10:04 -0700

    adding an Events mixin

---
+++ test/unit/events.js
@@ -0,0 +1,48 @@
+var Class = require('../../lib/class'),
+    Events = require('../../lib/class/Events'),
+    Spy = require('../testigo/lib/spy').Spy;
+
+module.exports = {
+    'Events': function(it, setup) {
+        
+        setup('beforeEach', function() {
+            this.E = new Events();
+        })
+
+        it('should be able to listen to events fired', function(expect) {
+            var fn = new Spy();
+            this.E.addEvent('tease', fn);
+            this.E.fireEvent('tease');
+            expect(fn.getCallCount()).toBe(1);
+        });
+
+        it('should be able to take an object map for addEvents', function(expect) {
+            var fn = new Spy();
+            this.E.addEvents({
+                'a': fn,
+                'b': fn
+            });
+
+            this.E.fireEvent('a');
+            this.E.fireEvent('b');
+            expect(fn.getCallCount()).toBe(2);
+        });
+
+        it('should be able to remove added event listeners', function(expect) {
+        
+            var fn = new Spy(),
+                fn2 = new Spy();
+            this.E.addEvent('a', fn);
+            this.E.addEvent('a', fn2);
+
+            this.E.fireEvent('a');
+
+            this.E.removeEvent('a', fn);
+            this.E.fireEvent('a');
+
+            expect(fn.getCallCount()).toBe(1);
+            expect(fn2.getCallCount()).toBe(2);
+        });
+
+    }
+};
