514805791766f58b17a4ab7113b967f4098c4766
Sean McArthur <sean.monstar@gmail.com>
2011-07-11 17:01:47 -0700

    forEach tests

--- test/unit/utils-object.js
+++ test/unit/utils-object.js
@@ -1,45 +1,79 @@
 var object = require('../../lib/utils/object');
 
 module.exports = {
-	
-	'merge': function(it, setup) {
-		
-		it('should merge any object inside the passed in object', function(expect) {
-			
-			var a = {a:1, b:2, c: {a:1, b:2, c:3}};
-			var b = {c: {d:4}, d:4};
-			var c = {a: 5, c: {a:5}};
-			
-			var merger = object.merge(a, b);
+    
+    'object.merge': function(it, setup) {
+        
+        it('should merge any object inside the passed in object', function(expect) {
+            
+            var a = {a:1, b:2, c: {a:1, b:2, c:3}};
+            var b = {c: {d:4}, d:4};
+            var c = {a: 5, c: {a:5}};
+            
+            var merger = object.merge(a, b);
 
-			expect(merger).toBeLike({a:1, b:2, c:{a:1, b:2, c:3, d:4}, d:4});
-			expect(merger === a).toBeTruthy();
-			
-			expect(object.merge(a, b, c)).toBeLike({a:5, b:2, c:{a:5, b:2, c:3, d:4}, d:4});
-		});
-		
-		it('should recursively clone sub objects and sub-arrays', function(expect){
-			var a = {a:1, b:2, c: {a:1, b:2, c:3}, d: [1,2,3]};
-			var b = {e: {a:1}, f: [1,2,3]};
-	
-			var merger = object.merge(a, b);
-	
-			expect(a.e === b.e).toBeFalsy();
-			expect(a.f === b.f).toBeFalsy();
-		})
-	},
-	
-	'clone': function(it, setup) {
-		it('should recursively clone and dereference arrays and objects', function(expect){
-			var a = {a:[1,2,3, [1,2,3, {a: [1,2,3]}]]};
-			var b = object.clone(a);
-			expect(a === b).toBeFalsy();
-			expect(a.a[3] === b.a[3]).toBeFalsy();
-			expect(a.a[3][3] === b.a[3][3]).toBeFalsy();
-			expect(a.a[3][3].a === b.a[3][3].a).toBeFalsy();
-	
-			expect(a).toBeLike(b);
-		});
-	}
-	
+            expect(merger).toBeLike({a:1, b:2, c:{a:1, b:2, c:3, d:4}, d:4});
+            expect(merger === a).toBeTruthy();
+            
+            expect(object.merge(a, b, c)).toBeLike({a:5, b:2, c:{a:5, b:2, c:3, d:4}, d:4});
+        });
+        
+        it('should recursively clone sub objects and sub-arrays', function(expect){
+            var a = {a:1, b:2, c: {a:1, b:2, c:3}, d: [1,2,3]};
+            var b = {e: {a:1}, f: [1,2,3]};
+    
+            var merger = object.merge(a, b);
+    
+            expect(a.e === b.e).toBeFalsy();
+            expect(a.f === b.f).toBeFalsy();
+        })
+    },
+    
+    'object.clone': function(it, setup) {
+        it('should recursively clone and dereference arrays and objects', function(expect){
+            var a = {a:[1,2,3, [1,2,3, {a: [1,2,3]}]]};
+            var b = object.clone(a);
+            expect(a === b).toBeFalsy();
+            expect(a.a[3] === b.a[3]).toBeFalsy();
+            expect(a.a[3][3] === b.a[3][3]).toBeFalsy();
+            expect(a.a[3][3].a === b.a[3][3].a).toBeFalsy();
+    
+            expect(a).toBeLike(b);
+        });
+    },
+
+    'object.forEach': function(it, setup) {
+        it('should call the function for each item in the object', function(expect){
+            var daysObj = {};
+            object.forEach({
+                first: "Sunday", 
+                second: "Monday", 
+                third: "Tuesday"}, function(value, key){
+                daysObj[key] = value;
+            });
+
+            expect(daysObj).toBeLike({
+                first: 'Sunday', 
+                second: 'Monday', 
+                third: 'Tuesday'
+            });
+        });
+
+        it('should ignore the prototype chain', function(expect){
+            var fn = function(){};
+            fn.prototype = {a: 1};
+
+            var obj = new fn;
+            obj.b = 2;
+
+            var items = {};
+            object.forEach(obj, function(value, key){
+                items[key] = value;
+            });
+
+            expect(items).toBeLike({b: 2});
+        });
+                
+    }
+    
 };
