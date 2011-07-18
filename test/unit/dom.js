334994aa35739c4c50b5be03bcf4b2db3ec64d5c
Sean McArthur <sean.monstar@gmail.com>
2011-07-18 11:32:37 -0700

    adding toElement method to dom.$

--- test/unit/dom.js
+++ test/unit/dom.js
@@ -1,38 +1,49 @@
 var dom = require('../../lib/dom');
 
 module.exports = {
-    
-    'Element': function(it, setup) {
-        it('should take a tagName in the constructor', function(expect) {
-            var el = new dom.Element('p');
-            expect(el).toBeAnInstanceOf(dom.Element);
-            expect(el.get('tag')).toBe('p');
-        });
-    },
-    
-    '$': function(it, setup) {
-        
-        it('should find an Element by id', function(expect) {
-            var el = dom.Document.createElement('p');
-            el.setAttribute('id', 'derp');
-            var body = dom.$$('body')[0];
-            body.appendChild(el);
-            
-            expect(dom.$('derp')).toBe(el);
+	
+	'Element': function(it, setup) {
+		it('should take a tagName in the constructor', function(expect) {
+			var el = new dom.Element('p');
+			expect(el).toBeAnInstanceOf(dom.Element);
+			expect(el.get('tag')).toBe('p');
+		});
+	},
+	
+	'$': function(it, setup) {
+		
+		it('should find an Element by id', function(expect) {
+			var el = dom.document.createElement('p');
+			el.setAttribute('id', 'derp');
+			var body = dom.$$('body')[0];
+			body.appendChild(el);
+			
+			expect(dom.$('derp')).toBe(el);
 
-            //el.destroy();
-        });
+			//el.destroy();
+		});
 
-    },
-    
-    '$$': function(it, setup) {
-        
-        it('should return Elements', function(expect) {
-            var els = dom.$$('body');
-            expect(els).toBeAnInstanceOf(dom.Elements);
-            expect(els.length).toBe(1);
-        });
+		it('should call toElement if passed an object', function(expect) {
+			var el = new dom.Element('p');
+			var obj = {
+				toElement: function() {
+					return el;
+				}
+			};
 
-    }
+			expect(dom.$(obj)).toBe(el);			
+		});
+
+	},
+	
+	'$$': function(it, setup) {
+		
+		it('should return Elements', function(expect) {
+			var els = dom.$$('body');
+			expect(els).toBeAnInstanceOf(dom.Elements);
+			expect(els.length).toBe(1);
+		});
+
+	}
 
 };
