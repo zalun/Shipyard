96dd063f63da8c1ff59bc4a14aa5e5548766ed97
Sean McArthur <sean.monstar@gmail.com>
2011-07-18 17:26:29 -0700

    adding attrs() helper for templates, will output View.attributes

--- test/unit/view.js
+++ test/unit/view.js
@@ -6,7 +6,16 @@ module.exports = {
 	'View': function(it, setup) {
 		it('should be able to render', function(expect) {
 			var v = new View({ data: 'test' });
+			delete v.attributes.id;
 			expect(v.render()).toBe('<span>test</span>');
+		});
+
+		it('should render attributes', function(expect) {
+			var v = new View;
+			delete v.attributes.id;
+			v.attributes['data-test'] = 'hello';
+
+			expect(v.render()).toBe('<span data-test="hello"></span>');
 		});
 
 	},
@@ -15,6 +24,10 @@ module.exports = {
 		it('should render child views', function(expect) {
 			var v = new Container;
 				v2 = new View({ data: 'test' });
+
+			delete v.attributes.id;
+			delete v2.attributes.id;
+
 
 			v.addView(v2);
 
@@ -25,6 +38,11 @@ module.exports = {
 			var c = new Container,
 				c2 = new Container({ data: 'contained' }),
 				v = new View({ data: 'test' });
+
+			delete c.attributes.id;
+			delete c2.attributes.id;
+			delete v.attributes.id;
+
 
 			c2.addView(v);
 			c.addView(c2);
