c1e695522bc35af5431a0c26a41cb2568e812a7d
Sean McArthur <sean.monstar@gmail.com>
2011-07-18 17:25:04 -0700

    adding serialize to elements

--- test/unit/dom.js
+++ test/unit/dom.js
@@ -9,6 +9,111 @@ module.exports = {
 			expect(el.get('tag')).toBe('p');
 		});
 	},
+
+	'Element.serialize': function(it, setup) {
+		it('should serialize text inputs', function(expect) {
+			var form = new dom.Element('form', { 
+				html: '<fieldset><input type="text" name="first_name" value="Sean" /><input type="text" name="last_name" value="McArthur" /><input type="text" name="title" /></fieldset>' 
+			});
+		
+			var data = form.serialize();
+
+			expect(data.first_name).toBe('Sean');
+			expect(data.last_name).toBe('McArthur');
+			expect(data.title).toBeFalsy();
+		});
+
+		it('should serialize checkboxes', function(expect) {
+			var form = new dom.Element('form', { html: '\
+				<fieldset>\
+					<input type="checkbox" name="topic" checked="checked" value="Javascript" />\
+					<input type="checkbox" name="topic" value="PHP" />\
+				</fieldset>\
+				<fieldset>\
+					<input type="checkbox" name="candy" checked="checked"  value="Twix" />\
+					<input type="checkbox" name="candy" checked="checked"  value="Snickers" />\
+				</fieldset>\
+				<fieldset>\
+					<input type="checkbox" name="sleepy" value="yes" checked="checked" />\
+				</fieldset>\
+			'});
+		
+			var data = form.serialize();
+			expect(data.topic).toBe('Javascript');
+			expect(data.sleepy).toBe('yes');
+			expect(data.candy).toBeType('array');
+			expect(data.candy.length).toBe(2);
+		});
+
+		it('should serialize radion buttons', function(expect) {
+			var form = new dom.Element('form', { html: '\
+				<fieldset>\
+					<input type="radio" checked="checked" value="Programmer" name="job" />\
+					<input type="radio" value="Designer" name="job" />\
+				</fieldset>\
+				<fieldset>\
+					<input type="radio" value="carls" name="food" />\
+					<input type="radio" value="tbell" name="food" />\
+				</fieldset>\
+			'});
+			
+			var data = form.serialize();
+
+			expect(data.job).toBe('Programmer');
+			expect(data.food).toBeFalsy();
+			expect(data.carls).toBeUndefined();
+		
+		});
+
+		it('should serialize textareas', function(expect) {
+			var form = new dom.Element('form', { 
+				html: '<fieldset><textarea name="t">La dee da.</textarea></fieldset>' 
+			});
+		
+			var data = form.serialize();
+			expect(data.t).toBe('La dee da.');
+			expect(data.f).toBeUndefined();
+		
+		});
+
+		it('should serialize select boxes', function(expect) {
+			var form = new dom.Element('form', { html: '\
+				<fieldset>\
+					<select name="job">\
+						<option selected="selected">Programmer</option>\
+						<option>Designer</option>\
+					</select>\
+					<select name="food">\
+						<option value="carls">Carls Jr</option>\
+						<option value="tbell" selected="selected">Taco Bell</option>\
+					</select>\
+				</fieldset>\
+			'});
+		
+			var data = form.serialize();
+			expect(data.job).toBe('Programmer');
+			expect(data.food).toBe('tbell');
+		});
+
+		it('should serialize multi selects', function(expect) {
+			var form = new dom.Element('form', { html: '\
+				<fieldset>\
+					<select name="food" multiple="multiple">\
+						<option value="carls">Carls Jr</option>\
+						<option value="tbell" selected="selected">Taco Bell</option>\
+						<option value="deltaco" selected="selected">Del Taco</option>\
+					</select>\
+				</fieldset>\
+			'});
+			
+			var data = form.serialize();
+
+			expect(data.food).toBeType('array');
+			expect(data.food.length).toBe(2);
+			expect(data.food[0]).toBe('tbell');
+			
+		});
+	},
 	
 	'$': function(it, setup) {
 		
