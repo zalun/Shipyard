4cb1dd1a20ec7130351b111522f3af051dce4e9e
Sean McArthur <sean.monstar@gmail.com>
2011-07-25 18:18:00 -0700

    Model.toJSON so that models can be serialized for syncing

--- test/unit/model.js
+++ test/unit/model.js
@@ -6,26 +6,26 @@ module.exports = {
 	
 	'Model': function(it, setup) {
 		
-        setup('before', function() {
-            this.User = new Class({
-                Extends: Model,
-                fields: {
-                    id: new Field({type: Number}),
-                    username: new Field({type: String}),
-                    password: new Field({type: String})
-                }
-            });
-        
-        })
+		setup('before', function() {
+			this.User = new Class({
+				Extends: Model,
+				fields: {
+					id: new Field({type: Number}),
+					username: new Field({type: String}),
+					password: new Field({type: String})
+				}
+			});
+		
+		})
 
 
 		it('should be able to set and get data', function(expect) {
-            var u = new this.User(),
-                name = 'johndoe';
-            u.set('username', name);
-            expect(u.get('username')).toBe(name);
-        
-        });
+			var u = new this.User(),
+				name = 'johndoe';
+			u.set('username', name);
+			expect(u.get('username')).toBe(name);
+		
+		});
 
 		it('should throw an Error if getting a non-existing field', function(expect) {
 			var u = new this.User();
@@ -41,47 +41,57 @@ module.exports = {
 			expect(err).toBeAnInstanceOf(Error);
 		});
 
-        it('should be able to take a hash to set data', function(expect) {
-            
-            var u = new this.User(),
-                props = {
-                    username: 'johndoe',
-                    password: 'pass1'
-                };
-            
-            u.set(props);
+		it('should be able to take a hash to set data', function(expect) {
+			
+			var u = new this.User(),
+				props = {
+					username: 'johndoe',
+					password: 'pass1'
+				};
+			
+			u.set(props);
 
-            expect(u.get('username')).toBe('johndoe');
-            expect(u.get('password')).toBe('pass1');
+			expect(u.get('username')).toBe('johndoe');
+			expect(u.get('password')).toBe('pass1');
 
-        });
+		});
 
-        it('should accept data in constructor', function(expect) {
-            var name = 'janedoe';
+		it('should accept data in constructor', function(expect) {
+			var name = 'janedoe';
 
-            var u = new this.User({
-                username: name,
-                password: 'derp'
-            });
+			var u = new this.User({
+				username: name,
+				password: 'derp'
+			});
 
-            expect(u.get('username')).toBe(name);
+			expect(u.get('username')).toBe(name);
 
-        });
+		});
 
-        it('should fire a propertyChange event when data changes', function(expect) {
-        
-            var u = new this.User();
+		it('should be serializable to JSON', function(expect) {
+			var obj = {
+				username: 'Sean',
+				password: 'derp'
+			};
+			var u = new this.User(obj);
+			
+			expect(JSON.stringify(u)).toBe(JSON.stringify(obj));
+		});
 
-            var nameChange = false;
+		it('should fire a propertyChange event when data changes', function(expect) {
+		
+			var u = new this.User();
 
-            u.addEvent('propertyChange', function(key, newVal, oldVal) {
-                if (key == 'username') nameChange = true;
-            });
+			var nameChange = false;
 
-            u.set('username', 'jenn');
-            expect(nameChange).toBe(true);
+			u.addEvent('propertyChange', function(key, newVal, oldVal) {
+				if (key == 'username') nameChange = true;
+			});
 
-        });
+			u.set('username', 'jenn');
+			expect(nameChange).toBe(true);
+
+		});
 		
 	}	
 	
