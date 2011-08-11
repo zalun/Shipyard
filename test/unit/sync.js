fb0653af499e68a29f3de4c649178a40d2a6f0da
Sean McArthur <sean.monstar@gmail.com>
2011-08-10 18:19:54 -0700

    Syncs can be chosen by passing the 'using' option, similar to Django.
    myModel.save({using: 'indexeddb'});

---
+++ test/unit/sync.js
@@ -0,0 +1,82 @@
+var Class = require('../../lib/class'),
+	Sync = require('../../lib/sync/Sync'),
+	Syncable = require('../../lib/sync/Syncable'),
+	Spy = require('../testigo/lib/spy').Spy;
+
+module.exports = {
+
+	'Syncable': function(it, setup) {
+		
+		var MockSync, MockSyncable;
+
+		setup('beforeEach', function() {
+			MockSync = new Class({
+				Extends: Sync,
+				initialize: function(options) {
+					this.parent(options);
+					this.read = new Spy;
+					this.create = new Spy;
+					this.update = new Spy;
+					this.destroy = new Spy;
+				}
+			});
+
+			MockSyncable = new Class({
+				Implements: Syncable
+			});
+		});
+		
+		it('should name Syncs and then use them with the "using" option', function(expect) {
+			var foo = new MockSync;
+			var bar = new MockSync;
+
+			MockSyncable.addSync('foo', foo);
+			MockSyncable.addSync('bar', bar);
+			MockSyncable.addSync('baz', new MockSync);
+
+			MockSyncable.find({}, {using: 'bar'});
+
+			expect(foo.read.getCallCount()).toBe(0);
+			expect(bar.read.getCallCount()).toBe(1);
+		});
+
+		it('should use "default" sync if "using" is not provided', function(expect) {
+			var foo = new MockSync;
+			var bar = new MockSync;
+
+			MockSyncable.addSync('default', foo);
+			MockSyncable.addSync('bar', bar);
+
+			MockSyncable.find();
+
+			expect(foo.read.getCallCount()).toBe(1);
+			expect(bar.read.getCallCount()).toBe(0);
+		})
+
+		it('should use the Sync mutator to add syncs', function(expect) {
+			var foo = new MockSync,
+				bar = new MockSync,
+				getFoo = function() { return foo; },
+				getBar = function() { return bar; };
+
+			var S = new Class({
+				Implements: Syncable,
+				Sync: {
+					'default': {
+						'driver': getFoo
+					},
+					'bar': {
+						'driver': getBar
+					}
+				}
+			});
+
+			S.find();
+			S.find(null, {using: 'bar'});
+
+			expect(foo.read.getCallCount()).toBe(1);
+			expect(bar.read.getCallCount()).toBe(1);
+
+		});
+	}
+};
