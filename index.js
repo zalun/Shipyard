e56c2e2a7ce4d4f9a2315f069b26a95c6f33cb82
Sean McArthur <sean.monstar@gmail.com>
2011-09-08 15:48:39 -0500

    shipyard build
    The build script has matured. It inspects the current directory for a 
    package.json, and uses the data from there to build a minified, single
    file of the app.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    ShipyardError = require('./lib/shipyard/error/Error');
 
 function loadPackage(dir) {
-    return JSON.parse(fs.readFileSync(path.join(dir, './package.json')));
+    var package = path.join(dir, './package.json');
+    try {
+        return JSON.parse(fs.readFileSync(package));
+    } catch (ex) {
+        throw new ShipyardError('Error parsing '+ package + '\n' + ex);
+    }
 }
 
 var shipyard = loadPackage(__dirname);
--- scripts/build.js
+++ scripts/build.js
@@ -1,58 +1,79 @@
 var path = require('path'),
-	copy = require('../../dryice').copy;
+    shipyard = require('../'),
+	copy = require('dryice').copy;
 
-var src = path.join(process.cwd(), process.argv[2]),
-	dir = path.dirname(src),
-	app = path.basename(src).replace(/\/$/, ''),
-	shipyard = path.join(__dirname, '../lib');
+exports.compile = function(appDir, dest) {
+    var pack = shipyard.loadPackage(appDir);
+        meta = pack.shipyard;
+    
+    console.log('Starting build of %s...', pack.name);
 
-var project = copy.createCommonJsProject({
-	roots: [dir, shipyard]
-});
+    var dir = path.join(appDir, '../'),
+        app = path.join(pack.name, meta.app),
+        shipyardDir = path.join(__dirname, '../lib');
 
+    console.log(dir, app);
+    var project = copy.createCommonJsProject({
+        roots: [dir, shipyardDir]
+    });
 
-var dest = path.join(process.cwd(), process.argv[3] || './'+app+'.js');
+    var build = copy.createDataObject();
+
+    // mini_require defaults to false, for now
+    if (meta.mini_require) {
+        console.log('Including mini require...');
+        copy({
+            source: [path.join(__dirname, '../build/require.js')],
+            dest: build
+        });
+    }
+    
+    console.log('Following all requires...');
+    copy({
+        source: copy.source.commonjs({
+            project: project,
+            require: [app]
+        }),
+        dest: build,
+        filter: [filterNode, wrapDefines]
+    });
+
+    copy({
+        source: [{
+            value: 'document.addEventListener("DOMContentLoaded", function() {require("' + app +'");}, false);'
+        }],
+        dest: build
+    });
+    
+    dest = dest || path.join(appDir, meta.min);
+    console.log('Copying to %s...', dest);
+    copy({
+        source: build,
+        dest: dest,
+        filter: []//[copy.filter.uglifyjs]
+    });
+
+    console.log('Done.');
+};
 
 function filterNode(content, location) {
-	if (typeof content !== 'string') content = content.toString();
-	return content.replace(/<node>.*<\/node>/g, '');
+    if (typeof content !== 'string') content = content.toString();
+    return content.replace(/<node>.*<\/node>/g, '');
 }
 filterNode.onRead = true;
 
 function wrapDefines(content, location) {
-	if (typeof content !== 'string') content = content.toString();
-	if (location.base) {
-		location = location.path;
-	}
-	location = location.replace(/(\.js|\/)$/, '');
-	return "define('"+ location +"', [], function(require, exports, module){\n" + content +"\n});\n";
+    if (typeof content !== 'string') content = content.toString();
+    if (location.base) {
+        location = location.path;
+    }
+    location = location.replace(/(\.js|\/)$/, '');
+    return "define('"+ location +"', [], function(require, exports, module){\n" + content +"\n});\n";
 }
 wrapDefines.onRead = true;
 
-var build = copy.createDataObject();
-copy({
-	source: [path.join(__dirname, '../build/require.js')],
-	dest: build
-});
-
-copy({
-	source: copy.source.commonjs({
-		project: project,
-		require: [app]
-	}),
-	dest: build,
-	filter: [filterNode, wrapDefines]
-});
-
-copy({
-	source: [{
-		value: 'document.addEventListener("DOMContentLoaded", function() {require("' + app +'");}, false);'
-	}],
-	dest: build
-});
-
-copy({
-	source: build,
-	dest: dest,
-	filter: [copy.filter.uglifyjs]
-});
+if (require.main == module) {
+    var src = path.join(process.cwd(), process.argv[2])
+        dest = path.join(process.cwd(), process.argv[3])
+    exports.compile(src, dest);
+}
