4970504b2ac8b903d293a624ff59e7fe57aa214d
Sean McArthur <sean.monstar@gmail.com>
2011-10-13 14:46:21 -0500

    fix dev-require to work with define, and play nice with ACE's require/define.
    its in need of a restructuring...

--- scripts/require.js
+++ scripts/require.js
@@ -6,28 +6,33 @@ function RequireError(msg) {
 RequireError.prototype = new Error;
 
 var XHR = function(){
-	try { return new XMLHttpRequest(); } catch(e){}
-	try { return new ActiveXObject('MSXML2.XMLHTTP'); } catch(e){}
-	try { return new ActiveXObject('Microsoft.XMLHTTP'); } catch(e){}
+    try { return new XMLHttpRequest(); } catch(e){}
+    try { return new ActiveXObject('MSXML2.XMLHTTP'); } catch(e){}
+    try { return new ActiveXObject('Microsoft.XMLHTTP'); } catch(e){}
 };
 
 var nonce = + new Date();
 var load = function(path){
-	if (path in load._cache) return load._cache[path];
-	var result = false;
-	var xhr = XHR();
-	xhr.open('GET', path + '?d=' + nonce, false);
-	xhr.send(null);
-	if (xhr.status >= 200 && xhr.status < 300) result = xhr.responseText;
+    if (path in load._cache) return load._cache[path];
+    var result = false;
+    var xhr = XHR();
+    xhr.open('GET', path + '?d=' + nonce, false);
+    xhr.send(null);
+    if (xhr.status >= 200 && xhr.status < 300) result = xhr.responseText;
 
-	return load._cache[path] = result;
+    return load._cache[path] = result;
 };
 load._cache = {};
 
 var compile = function(module, contents) {
-	module.exports = {};
-	var fn = new Function('exports, require, module, __filename, __dirname', contents)
-	require.paths.unshift(module.dirname);
+    module.exports = {};
+    var fn = new Function('require, exports, module, __filename, __dirname', contents)
+    require.paths.unshift(module.dirname);
+    exec(fn, module);
+    require.paths.shift();
+};
+
+var exec = function(fn, module) {
     var _req = function(id) {
         try {
             return require(id);
@@ -40,26 +45,26 @@ var compile = function(module, contents) {
         }
     };
     _req.paths = require.paths;
-    fn.call(window, module.exports, _req, module, module.filename, module.dirname);
-	require.paths.shift();
+    module.exports = {};
+    fn.call(window, _req, module.exports, module, module.filename, module.dirname);
 };
 
 var normalize = function(base, path){
-	if (path[0] == '/') base = '';
-	path = path.split('/').reverse();
-	base = base.split('/');
-	var last = base.pop();
-	if (last && !(/\.[A-Za-z0-9_-]+$/).test(last)) base.push(last);
-	var i = path.length;
-	while (i--){
-		var current = path[i];
-		switch (current){
-			case '.': break;
-			case '..': base.pop(); break;
-			default: base.push(current);
-		}
-	}
-	return base.join('/');
+    if (path[0] == '/') base = '';
+    path = path.split('/').reverse();
+    base = base.split('/');
+    var last = base.pop();
+    if (last && !(/\.[A-Za-z0-9_-]+$/).test(last)) base.push(last);
+    var i = path.length;
+    while (i--){
+        var current = path[i];
+        switch (current){
+            case '.': break;
+            case '..': base.pop(); break;
+            default: base.push(current);
+        }
+    }
+    return base.join('/');
 };
 
 var dirname = function(filename) {
@@ -75,50 +80,51 @@ var basename = function(filename) {
 var extname = function(filename) {
     var base = basename(filename);
     if (base.indexOf('.') > -1)
-	    return '.' + base.split('.').pop();
+        return '.' + base.split('.').pop();
 };
 
 
-var MODULES = {};
+var MODULES = (window.define && window.define.modules) || {};
 
 var tryFile = function(path) {
-	if (require._load(path)) return path;
-	return false;
+    if (require._load(path)) return path;
+    return false;
 };
 
 var tryExtensions = function(path, exts) {
-	var filename;
-	for (var i = 0; i < exts.length; i++) {
-		filename = path + exts[i];
-		if (tryFile(filename)) {
-			return filename;
-		}
-	}
-	return false;
+    var filename;
+    for (var i = 0; i < exts.length; i++) {
+        filename = path + exts[i];
+        if (tryFile(filename)) {
+            return filename;
+        }
+    }
+    return false;
 };
 
+var define = function(id, deps, factory) {
+    MODULES[id] = factory;
+}
+define.modules = MODULES;
+
 var require = function shipyard_require(id, path){
-	if (path) require.paths.unshift(path);
-	var contents = false,
-		filename,
-		ext = extname(id),
-		base = '',
-		paths = (id[0] === '/') ? [''] : require.paths,
-		trailingSlash = (id.slice(-1) === '/'),
+    if (path) require.paths.unshift(path);
+    var contents = false,
+        filename,
+        ext = extname(id),
+        base = '',
+        paths = (id[0] === '/') ? [''] : require.paths,
+        trailingSlash = (id.slice(-1) === '/'),
         isRelative = id.charAt(0) == '.',
-		module;
-	
-	if (require.original) {
-		var orig = require.original(id);
-		if (orig) {
-			module = { filename: id, dirname: dirname(id), exports: orig };
-			MODULES[id] = module;
-		}
-	} 
-	if (!module) {
-		var exts = Object.keys(require.extensions);
-		for (var i = 0, y = paths.length; (i < y); i++) {
-			if (isRelative) {
+        module = MODULES[id];
+    
+
+
+
+    if (!module) {
+        var exts = Object.keys(require.extensions);
+        for (var i = 0, y = paths.length; (i < y); i++) {
+            if (isRelative) {
                 base = normalize(paths[i], id);
             } else {
                 // check path versus id
@@ -134,78 +140,96 @@ var require = function shipyard_require(id, path){
                 }
             }
 
-			module = MODULES[base];
-			if(module)  break;
-			
-			//1. tryFile
-			//2. tryExtensions
-			//3. tryExtensions with index
-			if (!trailingSlash) {
+            module = MODULES[base];
+            if(module)  break;
+            
+            //1. tryFile
+            //2. tryExtensions
+            //3. tryExtensions with index
+            if (!trailingSlash) {
                 if (ext) {
-				    filename = tryFile(base);
+                    filename = tryFile(base);
                 }
-				if (!filename) {
-				    filename = tryExtensions(base, exts);
-				}
+                if (!filename) {
+                    filename = tryExtensions(base, exts);
+                }
                 if (!filename && !ext) {
                     filename = tryFile(base);
                 }
-			}
+            }
 
-			if (!filename) {
-				filename = tryExtensions(normalize(base, 'index'), exts);
-			}
+            if (!filename) {
+                filename = tryExtensions(normalize(base, 'index'), exts);
+            }
 
-			if (filename !== false) {
-				module = { filename: filename, dirname: dirname(filename) };
-				ext = extname(filename) || '.js';
-				if (!require.extensions[ext]) ext = '.js'
-				require.extensions[ext](module, filename);
-				MODULES[base] = module;
-				break;
-			}
-		}
-	}
-	if (!module) throw new RequireError('Cannot find module "' + id + '"');
-	if (path) require.paths.shift();
-	return module.exports;
+            if (filename !== false) {
+                module = { filename: filename, dirname: dirname(filename) };
+                ext = extname(filename) || '.js';
+                if (!require.extensions[ext]) ext = '.js'
+                require.extensions[ext](module, filename);
+                MODULES[base] = module;
+                break;
+            }
+        }
+    }
+    if (!module && require.original) {
+        var orig = require.original(id);
+        if (orig) {
+            return orig;
+        }
+    }
+    if (!module) throw new RequireError('Cannot find module "' + id + '"');
+    if (path) require.paths.shift();
+    if (typeof module === 'function') {
+        var factory = module;
+        module = { filename: id, dirname: dirname(id) };
+        exec(factory, module);
+        MODULES[id] = module;
+    }
+    if (module && !module.exports) {
+        module.exports = module;
+    }
+    return module.exports;
 };
 
 require.extensions = {
-	'.js': function(module, filename) {
-				require._compile(module, require._load(filename));
-	}
+    '.js': function(module, filename) {
+                require._compile(module, require._load(filename));
+    }
 };
 
 require._load = load;
 require._compile = compile;
-require._modules = MODULES;
 require.paths = [window.location.pathname];
 
 
 if (window.require)
-	require.original = window.require;
+    require.original = window.require;
+if (window.define)
+    define.original = window.define;
 
 window.require = require;
+window.define = define;
 
 //find script with data-main
-window.addEventListener('DOMContentLoaded', function() {
-	var scripts = document.getElementsByTagName('script'),
-		main;
-	for (var i = 0, length = scripts.length; i < length; i++) {
-		var script = scripts[i],
-            src = script.getAttribute('src');
-        if ((src.indexOf('require.js') >= 0) && (main = script.getAttribute('data-main'))) {
-			var maindir = dirname(main),
-                shipyard = normalize(dirname(src), '../lib/shipyard');
-            require.paths.unshift(shipyard);
-			if (~require.paths.indexOf(maindir))
-				require.paths.unshift(maindir);
-			require(main);
-			break;
-		}
-	}
+var scripts = document.getElementsByTagName('script'),
+    main;
+for (var i = 0, length = scripts.length; i < length; i++) {
+    var script = scripts[i],
+        src = script.getAttribute('src');
+    if ((src.indexOf('require.js') >= 0) && (main = script.getAttribute('data-main'))) {
+        var maindir = dirname(main),
+            shipyard = normalize(dirname(src), '../lib/shipyard');
+        require.paths.unshift(shipyard);
+        if (!~require.paths.indexOf(maindir)) {
+            require.paths.unshift(maindir);
+        }
+        break;
+    }
+}
 
+window.addEventListener('DOMContentLoaded', function() {
+    require(main);
 }, false);
 
 })();
