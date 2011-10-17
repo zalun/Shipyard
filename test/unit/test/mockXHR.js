7399f8e7c5ce02ed69dfb3764c52a20b305c92d8
Sean McArthur <sean.monstar@gmail.com>
2011-10-17 18:47:56 -0500

    adds mockXHR to `shipyard.test`
    mockXHR takes a response argument, and an optional status argument. mockXHR also will keep track of each call in a fifo manner.
    
    fixes #3

---
+++ test/unit/test/mockXHR.js
@@ -0,0 +1,44 @@
+var mockXHR = require('../../../lib/shipyard/test/mockXHR'),
+    Request = require('../../../lib/shipyard/http/Request');
+
+module.exports = {
+    'mockXHR': function(it, setup) {
+        it('should mock the next request', function(expect) {
+            mockXHR('ok');
+            new Request({
+                onComplete: function(text) {
+                    expect(text).toBe('ok');
+                }
+            }).send();
+        });
+
+        it('should keep order of mocks', function(expect) {
+            mockXHR('one');
+            mockXHR('two');
+
+            new Request({
+                onComplete: function(text) {
+                    expect(text).toBe('one');            
+                }
+            }).send();
+
+            new Request({
+                onComplete: function(text) {
+                    expect(text).toBe('two');
+                }
+            }).send();
+        });
+
+        it('should be able to mock statuses', function(expect) {
+            mockXHR('error', 500);
+            var r = new Request({
+                onFailure: function(text) {
+                    expect(this.status).toBe(500);
+                    expect(text).toBe('error');
+                }
+            });
+            
+            r.send();
+        });
+    }
+};
